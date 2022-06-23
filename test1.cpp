6354d3b22661ab097ea62c7d840a93633d30e2f4
ScuterYoung <1435359352@qq.com>
2022-06-23 13:17:48 +0800

    Initial commit

---
+++ test1.cpp
@@ -0,0 +1,27 @@
+//
+// Created by hp on 22.6.20.
+//
+// #include <iostream>
+
+// using namespace std;
+
+// int main2() {
+//     int a;
+//     cout << "ÊäÈëÊı×Ö£º" << endl;
+//     cin >> a;
+//     cout << a << "Hello, test1!" << endl; //×¢ÊÍ
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "ÊäÈëÊı×Ö£º" << endl;
+    cin >> a;
+    cout << a << "Hello, test1!" << endl; //×¢ÊÍ
+
+    cout << endl;
+    return 0;
+}
